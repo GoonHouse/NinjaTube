8a4b51f64b5d42a5bec32a418a5ec58d766b54e5
TomRintjema <tomrintjema@hotmail.com>
2016-08-05 08:51:08 -0400

    Init.

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position - Vector3.forward * Time.deltaTime * speed;
+	}
+}
