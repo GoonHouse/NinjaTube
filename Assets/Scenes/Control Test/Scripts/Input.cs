086d3ffca3a5087d592a454faee896d8c1e7dd12
TomRintjema <tomrintjema@hotmail.com>
2016-08-18 15:25:22 -0400

    got player moving and pointing "up" in the tube. Still needs movement to make sense and also needs gravity for the player but before I do that I have to make it so the player has something to hit and not fall forever.

--- Assets/Scenes/Control Test/Scripts/Input.cs
+++ Assets/Scenes/Control Test/Scripts/Input.cs
@@ -5,8 +5,11 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Input : MonoBehaviour {
 	public Transform world;
 	public Transform playerMoveTarget;
+	public Transform player;
 	public float targetSpeed = 10f;
 	public float returnSpeed = 1f;
+	public float playerSpeed = 1f;
+	public float playerRotationSpeed = 1f;
 
 	// Use this for initialization
 	void Start () {
@@ -24,7 +27,14 @@ public class Input : MonoBehaviour {
 		transform.position = world.transform.position + inputVector; 
 		playerMoveTarget.transform.position += inputVector * Time.deltaTime * targetSpeed;
 
-		Vector3 gravity = Vector3.MoveTowards (playerMoveTarget.transform.position, world.transform.position, 0.01f);
-		playerMoveTarget.transform.position -= gravity * Time.deltaTime * returnSpeed;
+		Vector3 moveTargetGravity = Vector3.MoveTowards (playerMoveTarget.transform.position, world.transform.position, 0.01f);
+		playerMoveTarget.transform.position -= moveTargetGravity * Time.deltaTime * returnSpeed;
+
+		Vector3 gravity = Vector3.MoveTowards (player.transform.position, world.transform.position, 0.01f);
+
+		Vector3 directionToMoveTarget = Vector3.Cross (moveTargetGravity, Vector3.forward); //this is wrong, needs work
+
+		player.transform.position += directionToMoveTarget * Time.deltaTime * playerSpeed;
+		player.transform.rotation = Quaternion.LookRotation (Vector3.forward, -gravity);
 	}
 }
